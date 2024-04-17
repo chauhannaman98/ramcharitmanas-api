0b77f8123e050df0ab99adbf8a0f227e1065d391
Naman Chauhan <chauhannaman98@gmail.com>
2024-04-17 19:47:04 +0530

    added tests

---
+++ mock/mock_root_response.py
@@ -0,0 +1,13 @@
+root_response = {
+        "message": "Jai Shree Ram!",
+        "docs": "/docs",
+        "chapters_available": "1, 2",
+        "enpoints-available": {
+            "chapter": [
+            "/chapters",
+            "/chapters/{chapter_number}",
+            "/chapters/{chapter_number}/verses",
+            "/chapters/{chapter_number}/verses/{verse_number}"
+            ]
+        }
+    }
