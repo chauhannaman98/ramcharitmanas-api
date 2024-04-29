4e30bd2fe621c55ded20dcdb4285bd6021d4035d
Naman Chauhan <chauhannaman98@gmail.com>
2024-04-29 20:19:18 +0530

    Added redis to get_all_verses

--- res/chapter.py
+++ res/chapter.py
@@ -3,8 +3,17 @@ from schemas import ChapterModel, VerseModel
 from database.models import ManasChapter, ManasVerse
 from fastapi import HTTPException, status, Response
 from typing import List
+import json
+import ast
+from res.row2dict import row2dict
+import redis
 
 
+rd = redis.Redis(
+    host="redis-13321.c212.ap-south-1-1.ec2.redns.redis-cloud.com",
+    port=13321,
+    password='CyiJc7g00862vnSfcnz3qBHtctnzREF9'
+    )
 
 # method returns a list of all the chapters
 def get_all_chapters(db: Session) -> List:
@@ -30,19 +39,33 @@ def get_chapter(db: Session, chapter_number: int) -> ChapterModel:
 
 # method returns a list of verses in a chapter
 def get_all_verses(db: Session, chapter_number: int):
-    verses = db.query(ManasVerse).filter(
-        ManasVerse.chapter_number == chapter_number
-    ).order_by(
-        ManasVerse.verse_number.asc()
-    ).all()
+    redis_key = f"chapter_{chapter_number}"
+    cached_data = rd.get(redis_key)
 
-    if verses:
-        return verses
-    
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail=f"No data found for chapter {str(chapter_number)}."
-    )
+    if cached_data:
+        cached_data_str = cached_data.decode('utf-8')
+        cached_data_list = ast.literal_eval(cached_data_str)
+        return cached_data_list
+    else:
+        verses = db.query(ManasVerse).filter(
+            ManasVerse.chapter_number == chapter_number
+        ).order_by(
+            ManasVerse.verse_number.asc()
+        ).all()
+
+        if verses:
+            parsed_list = []
+            for verse in verses:
+                parsed_list.append(row2dict(verse))
+
+            rd.set(redis_key, str(parsed_list))
+            rd.expire(redis_key, 60)
+            return parsed_list
+        
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"No data found for chapter {str(chapter_number)}."
+        )
 
 
 # returns the particular verse from the chapter
