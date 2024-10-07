f2992f72c96dd43c0d04a01d9e1a9c16252fb208
Naman Chauhan <chauhannaman98@gmail.com>
2024-10-07 18:57:25 +0530

    added exception handling

--- res/chapter.py
+++ res/chapter.py
@@ -27,51 +27,84 @@ except redis.ConnectionError as e:
 
 # method returns a list of all the chapters
 def get_all_chapters(db: Session) -> List:
-    chapters = db.query(ManasChapter).order_by(
-        ManasChapter.chapter_number.asc()
-    ).all()
+    try:
+        chapters = db.query(ManasChapter).order_by(
+            ManasChapter.chapter_number.asc()
+        ).all()
+    except:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Failed to retrieve data from database."
+        )
 
     return chapters
 
 
 # method return a chapter
 def get_chapter(db: Session, chapter_number: int) -> ChapterModel:
-    chapter = db.query(ManasChapter).filter(ManasChapter.chapter_number == chapter_number).first()
-
-    if chapter is None:
+    try:
+        chapter = db.query(ManasChapter).filter(
+            ManasChapter.chapter_number == chapter_number
+            ).first()
+    except:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Chapter number {str(chapter_number)} does not exist"
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Failed to retrieve data from database."
         )
+
+    if chapter:
+        return chapter
     
-    return chapter
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"Chapter number {str(chapter_number)} does not exist"
+    )
 
 
 # method returns a list of verses in a chapter
 def get_all_verses(db: Session, chapter_number: int):
     redis_key = f"chapter_{chapter_number}"
-    cached_data = rd.get(redis_key)
+
+    try:
+        cached_data = rd.get(redis_key)
+    except:
+        raise HTTPException(
+            status_code = status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Failed to retrieve key {str(redis_key)} from cache."
+        )
 
     if cached_data:
         cached_data_str = cached_data.decode('utf-8')
         cached_data_list = ast.literal_eval(cached_data_str)
         return cached_data_list
     else:
-        verses = db.query(ManasVerse).filter(
-            ManasVerse.chapter_number == chapter_number
-        ).order_by(
-            ManasVerse.verse_number.asc()
-        ).options(
-            joinedload(ManasVerse.translations)
-        ).all()
+        try:
+            verses = db.query(ManasVerse).filter(
+                ManasVerse.chapter_number == chapter_number
+            ).order_by(
+                ManasVerse.verse_number.asc()
+            ).options(
+                joinedload(ManasVerse.translations)
+            ).all()
+        except:
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Failed to retrieve data from database."
+            )
 
         if verses:
             parsed_list = []
             for verse in verses:
                 parsed_list.append(row2dict(verse))
 
-            rd.set(redis_key, str(parsed_list))
-            rd.expire(redis_key, os.getenv('REDIS_CHAPTER_CACHE_TIMEOUT'))
+            try:
+                rd.set(redis_key, str(parsed_list))
+                rd.expire(redis_key, os.getenv('REDIS_CHAPTER_CACHE_TIMEOUT'))
+            except:
+                raise HTTPException(
+                    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                    detail=f"Failed to set data to redis cache with key: {str(redis_key)}."
+                )
             return parsed_list
         
         raise HTTPException(
@@ -82,13 +115,16 @@ def get_all_verses(db: Session, chapter_number: int):
 
 # returns the particular verse from the chapter
 def get_verse_from_chapter(db: Session, chapter_num: int, verse_num: int) -> VerseModel:
-    print("### calling get_verse_from_chapter ###")
-    verse = db.query(ManasVerse).filter(
-        ManasVerse.chapter_number == chapter_num,
-        ManasVerse.verse_number == verse_num
-    ).first()
-    
-    print(verse)
+    try:
+        verse = db.query(ManasVerse).filter(
+            ManasVerse.chapter_number == chapter_num,
+            ManasVerse.verse_number == verse_num
+        ).first()
+    except:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Failed to retrieve data from database."
+        )
 
     if verse:
         return verse
